9f726c56cd7861bb5e2168818d58cbc0304bb560
chisomnzube <chisomnzube@gmail.com>
2023-02-04 15:05:01 +0100

    first commit

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <section>
+        <base-card>
+            <h2>Page not found</h2>
+            <p>This page could not be found. Maybe check out our <router-link to="/coaches">Coaches link</router-link> </p>
+        </base-card>
+    </section>
+</template>
