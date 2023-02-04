c6c8ecaa1af26bbb19275d0e090a977dc4bc83d0
chisomnzube <chisomnzube@gmail.com>
2023-02-04 15:15:12 +0100

    changes

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -15,10 +15,10 @@ export default {
     },
     async auth(context, payload) {
         const mode = payload.mode;
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBRMFvJEE8feSQInM6AzlUzkffdjoUMSz0'
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=YOUR_API_KEY'
 
         if (mode === 'signup') {
-            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBRMFvJEE8feSQInM6AzlUzkffdjoUMSz0'
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=YOUR_API_KEY'
         }
         const response = await fetch(url, {
             method: 'POST',
